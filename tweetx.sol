aca548ced1296ab3c4d063787f44098946346fb4
maxdesalle <max@sablier.finance>
2023-06-11 10:48:14 +0200

    fix: change function name

--- tweetx.sol
+++ tweetx.sol
@@ -20,8 +20,8 @@ contract TweetX is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Supply {
         _setURI(newuri);
     }
 
-    function setURI(string memory newuri, uint256 id) public onlyOwner {
-				_tokenURIs[id] = tokenURI;
+    function updateURI(string memory newuri, uint256 id) public onlyOwner {
+				_tokenURIs[id] = newuri;
     }
 
     function pause() public onlyOwner {
